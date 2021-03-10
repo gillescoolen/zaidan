87bcae1005a64bcba419b96708f980104400cee4
Raul de Heer <raul@supertask.nl>
2021-03-10 15:38:26 +0100

    Add authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
 import { validate } from './util/env.validation';
+import { JwtStrategy } from './auth/strategies/jwt.strategy';
 
 @Module({
-  imports: [ConfigModule.forRoot({ validate }), DatabaseModule],
+  imports: [ConfigModule.forRoot({ validate }), DatabaseModule, UserModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService, JwtStrategy]
 })
 export class AppModule {}
