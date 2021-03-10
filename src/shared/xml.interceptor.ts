0e240e9d2d4a935ae450de66e4c4a7a04f6419c9
Gilles <gilles@coolen.dev>
2021-03-10 21:22:23 +0100

    Add XML interceptor

---
+++ src/shared/xml.interceptor.ts
@@ -0,0 +1,15 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import * as jsontoxml from 'jsontoxml';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class XMLInterceptor<T> implements NestInterceptor<T, string> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<string> {
+    const ctx = context.switchToHttp();
+    const request = ctx.getRequest<Request>();
+    const requestedXML = request.headers['content-type'] === 'text/xml';
+
+    return next.handle().pipe(map((data) => (requestedXML && typeof data === 'object' ? jsontoxml(data) : data)));
+  }
+}
