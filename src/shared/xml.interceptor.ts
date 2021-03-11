bb3185f686a417733266bff7857e62ecd9d7fd8a
Gilles <gilles@coolen.dev>
2021-03-11 11:06:20 +0100

    Re-order variables

--- src/shared/xml.interceptor.ts
+++ src/shared/xml.interceptor.ts
@@ -6,9 +6,9 @@ import { map } from 'rxjs/operators';
 @Injectable()
 export class XMLInterceptor<T> implements NestInterceptor<T, string> {
   intercept(context: ExecutionContext, next: CallHandler): Observable<string> {
+    const regex = /\/xml/gi;
     const ctx = context.switchToHttp();
     const request = ctx.getRequest<Request>();
-    const regex = /\/xml/gi;
     const requestedXML = request.headers['content-type'].match(regex);
 
     return next.handle().pipe(map((data) => (requestedXML && typeof data === 'object' ? jsontoxml(data) : data)));
