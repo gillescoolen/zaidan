7f214ffb18cf82a323624a5c52f997af19956a9b
Gilles <gilles@coolen.dev>
2021-03-11 12:58:37 +0100

    Move interceptor to interceptor folder

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as rateLimit from 'express-rate-limit';
 import * as helmet from 'helmet';
 import { AppModule } from './app.module';
-import { XMLInterceptor } from './common/xml.interceptor';
+import { XMLInterceptor } from './common/interceptors/xml.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
